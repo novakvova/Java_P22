1224bf19d00e32fd00cf254eb26dfbe13d032f55
novakvova <novakvova@gmail.com>
2025-11-18 20:55:48 +0200

    Woking is good add taiwind css

---
+++ 7.Working API/reat-tran/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import MainHeader from "./layout/Main/MainHeader.tsx";
+
+function App() {
+
+  return (
+    <>
+        <MainHeader/>
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
