c1f8c979429961d497fea89376b91139b54cf7df
novakvova <novakvova@gmail.com>
2025-11-20 20:57:04 +0200

    Working frond and java View site

--- 7.Working API/reat-tran/src/App.tsx
+++ 7.Working API/reat-tran/src/App.tsx
@@ -1,14 +1,28 @@
 import './App.css'
-import MainHeader from "./layout/Main/MainHeader.tsx";
+import {Route, Routes} from "react-router";
+import MainLayout from "./layout/Main/MainLayout.tsx";
+import NotFoundPage from "./pages/common/NotFoundPage.tsx";
+import UserHomePage from "./pages/user/UserHomePage";
+import RegisterPage from "./pages/account/RegisterPage";
+import CreateCountryPage from "./pages/country/CreateCountryPage";
 
 function App() {
 
-  return (
-    <>
-        <MainHeader/>
-      <h1>Hello</h1>
-    </>
-  )
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<MainLayout />}>
+                    <Route index element={<UserHomePage/>} />
+
+                    <Route path="/countries/create" element={<CreateCountryPage />} />
+
+                    <Route path={"/register"} element={<RegisterPage/>} />
+                </Route>
+
+                <Route path="*" element={<NotFoundPage/>} />
+            </Routes>
+        </>
+    )
 }
 
 export default App
--- 7.Working API/transportation/src/main/java/org/example/config/WebConfig.java
+++ 7.Working API/transportation/src/main/java/org/example/config/WebConfig.java
@@ -1,6 +1,7 @@
 package org.example.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,5 +15,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/"+uploadDir+"/**")
                 .addResourceLocations("file:"+uploadDir+"/");
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
